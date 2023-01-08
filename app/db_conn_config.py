a03c03caf49ebf27217f540c919a4fda58455c1f
Reshma Thomas <reshmathomas1194@gmail.com>
2023-01-08 12:15:37 -0800

    IMPLEMENTATION: Connect to a MySQL DB server instance
    Established connection to a MySQL server instance and created database and table to store all
    malware URLs. The service now checks the table of malware URLs to know whether the url in the GET
    request is malware or NOT.

---
+++ app/db_conn_config.py
@@ -0,0 +1,12 @@
+import MySQLdb
+
+mysql_host = "localhost"
+username = "root"
+password = "root123##"
+
+db = MySQLdb.connect(mysql_host, username, password)
+cursor = db.cursor()
+cursor.execute("CREATE DATABASE if not exists malware_urls;")
+cursor.execute("USE malware_urls;")
+query = "CREATE TABLE if not exists malware_url_info (url VARCHAR(512));"
+cursor.execute(query)
