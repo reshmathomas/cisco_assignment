159d32a19ecad44be35ea56781638e8a8b58b075
Reshma Thomas <reshmathomas1194@gmail.com>
2023-01-08 09:25:20 -0800

    IMPLEMENTATION: Create a web service to parse GET /v1/urlinfo?url=<url> REST requests
    The service validates whether the URL is in a proper format and raises exception if not.
    It strips off the "domain.tld" part from the URL to match against the DB entries so that
    https://www.domain.tld is treated the same as www.domain.tld.

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from malware_url_lookup_service import malware_url_lookup_service
+
+app = Flask(__name__)
+
+@app.route('/v1/urlinfo/', methods=['GET'])
+def get():
+    url = request.args.get('url')
+    try:
+        is_url_malware = malware_url_lookup_service(url).validate()
+    except:
+        print "Error occured during validation of url."
+        raise
+
+    if is_url_malware:
+        return "Malware!"
+    else:
+        return "Not a malware!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
